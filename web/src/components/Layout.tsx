ad6da0437d2846b9e787c23b15908bf93f45eebb
jplps <joao.pedro.pacheco@hotmail.com>
2020-06-04 09:33:41 -0300

    Refactored to spectrum

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-
-import Header from './Header';
+import { Link } from 'react-router-dom';
 
 const Layout: React.FC = ({ children }) => (
 	<div className="grid">
-		<Header title={'artLAB'} />
+		<Link to='/'>
+			<img
+				style={{
+					width: '100%',
+					height: 'auto'
+				}}
+				src="http://localhost:4000/assets/logo.png"
+				alt="Spectrum Logo"
+			/>
+		</Link>
 		<p>Register art events all over the world, and locate the ones you want.</p>
 
 		{children}
-	</div>
+	</div >
 )
 
 export default Layout;
